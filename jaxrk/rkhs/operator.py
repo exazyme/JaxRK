1b747b59b7f85f57cea7fdc340efa1784d61b88f
Ingmar Schuster <ingmar.schuster@zalando.de>
2021-06-24 09:01:20 +0200

    Added RidgeCMO – closed form CMO fit from literature

--- jaxrk/rkhs/operator.py
+++ jaxrk/rkhs/operator.py
@@ -176,6 +176,15 @@ def Cmo(inp_feat:InpVecT, outp_feat:OutVecT, regul:float = None) -> FiniteOp[Inp
             assert regul.squeeze().size == 1 or regul.squeeze().shape[0] == len(inp_feat)
         return  CrossCovOp(Cov_solve(CovOp(inp_feat), inp_feat, regul=regul), outp_feat)
 
+def RidgeCmo(inp_feat:InpVecT, outp_feat:OutVecT, regul:float = None) -> FiniteOp[InpVecT, OutVecT]:
+        if regul is None:
+            regul = Cov_regul(1, len(inp_feat))
+        else:
+            regul = np.array(regul, dtype=np.float32)
+            assert regul.squeeze().size == 1 or regul.squeeze().shape[0] == len(inp_feat)
+        matr = np.linalg.inv(inp_feat.inner() + regul * np.eye(len(inp_feat)))
+        return  FiniteOp(inp_feat, outp_feat, matr)
+
 def Cdo(inp_feat:InpVecT, outp_feat:OutVecT, ref_feat:OutVecT, regul = None) -> FiniteOp[InpVecT, OutVecT]:
         if regul is not None:
             regul = np.array(regul, dtype=np.float32)
