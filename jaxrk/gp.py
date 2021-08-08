e10a6ca10e3ede42a626979a31d035067e1b4c9c
Ingmar Schuster <ingmar.schuster@gmail.com>
2021-08-08 11:18:30 +0200

    Recheck GP implementation - adopt einsum as in sklearn - introduce scaling of output

--- jaxrk/gp.py
+++ jaxrk/gp.py
@@ -4,30 +4,40 @@ import jax.numpy as np, flax.linen as ln, jax.scipy as scipy
 from typing import Any
 
 class GP(object):
-    def __init__(self, x:FiniteVec, y:Array, noise:float, amp:float, kconst:float = 0.):
-        self.x, self.y, self.noise, self.amp, self.kconst = ( x, y, noise, amp, kconst)
-        self.ymean = np.mean(y)
-        self.y -= self.ymean
-        train_cov = self.amp * (self.x.inner() + kconst) + np.eye(len(self.x)) * self.noise 
+    def __init__(self, x:FiniteVec, y:Array, noise:float, normalize_y:bool = False):
+        self.x, self.y, self.noise, self.normalize_y = (x, y, noise, normalize_y)
+        if self.y.ndim == 1:
+            self.y = self.y[:, np.newaxis]
+        if not normalize_y:
+            self.ymean = np.zeros(1)
+            self.ystd = np.ones(1)
+        else:
+            self.ymean = np.mean(y, 0, keepdims=True)
+            self.ystd = np.std(y, 0, keepdims=True)
+            self.y = (self.y - self.ymean) / self.ystd
+
+        train_cov = self.x.inner() + np.eye(len(self.x)) * self.noise 
         self.chol = scipy.linalg.cholesky(train_cov, lower=True)
-        self.kinvy = scipy.linalg.solve_triangular(self.chol.T, scipy.linalg.solve_triangular(self.chol, self.y, lower=True))
+        self.alpha = scipy.linalg.cho_solve((self.chol, True), self.y)
+        self.log2pi_half = np.log(2. * np.pi) / 2
+
     
-    def marginal_likelihood(self):
-        log2pi = np.log(2. * np.pi)
-        ml = np.sum(
-            -0.5 * np.dot(self.y.T, self.kinvy) -
-            np.sum(np.log(np.diag(self.chol))) -
-            (len(self.x) / 2.) * log2pi)
-        ml -= np.sum(-0.5 * log2pi - np.log(self.amp)**2) # lognormal prior
+    def marginal_likelihood(self):        
+        ml = np.sum(- 0.5 * np.einsum("ik,ik->k", self.y, self.alpha)
+                    - np.log(np.diag(self.chol)).sum()
+                    - len(self.x) * self.log2pi_half)
+        ml -= np.sum(-self.log2pi_half) # lognormal prior
         return -ml
 
     def predict(self, xtest:FiniteVec = None):
-        cross_cov = self.amp*(self.x.inner(xtest) + self.kconst)
-        mu = np.dot(cross_cov.T, self.kinvy) + self.ymean
-        v = scipy.linalg.solve_triangular(self.chol, cross_cov, lower=True)
-        var = (self.amp * (xtest.inner() + self.kconst) - np.dot(v.T, v))
-        return mu, var
+        cross_cov = self.x.inner(xtest)
+        mu = self.ystd * np.dot(cross_cov.T, self.alpha) + self.ymean
+        v = scipy.linalg.cho_solve((self.chol, True), cross_cov)
+        cov = (xtest.inner() - np.dot(cross_cov.T, v)) * self.ystd**2
+        return mu, cov
     
     def post_pred_likelihood(self, xtest:FiniteVec, ytest:Array):
         m, v = self.predict(xtest)
+        if self.normalize_y:
+            ytest = (ytest - self.ymean) / self.ystd
         return m, v, scipy.stats.multivariate_normal.logpdf(ytest.ravel(), m.ravel(), v)
