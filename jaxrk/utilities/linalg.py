f528879fea65da2ab1d85156b12e5c2911159a00
Ingmar Schuster <ingmar.schuster@gmail.com>
2022-01-24 10:25:08 +0100

    Comments for linalg functions.

--- jaxrk/utilities/linalg.py
+++ jaxrk/utilities/linalg.py
@@ -4,9 +4,30 @@ import numpy.random as random
 
 
 def block_matrix(UL:Array, UR:Array, LL:Array, LR:Array) -> Array:
+    """Correctly stack a block matrix from its parts.
+
+    Args:
+        UL (Array): Upper left part (shape N x M) of the resulting matrix.
+        UR (Array): Upper right part (shape N x L) of the resulting matrix.
+        LL (Array): Upper left part (shape K x M)of the resulting matrix.
+        LR (Array): Upper right part (shape K x L) of the resulting matrix.
+
+    Returns:
+        Array: Resulting matrix from stacking parts, shape (N+K) x (M+L).
+    """
     return np.vstack([np.hstack([UL, UR]), np.hstack([LL, LR])])
 
 def augment_gram(G_old:Array, G_old_new:Array, G_new:Array)-> Array:
+    """Correctly stack a gram matrix from its parts of old and new kernel evaluations.
+
+    Args:
+        G_old (Array): Gram matrix involving only old data points (shape N x N).
+        G_old_new (Array): Gram matrix involving old data and new data points (shape N x M).
+        G_new (Array): Gram matrix involving only new data points (shape M x M).
+
+    Returns:
+        Array: Resulting gram matrix from stacking parts, shape (N+M) x (N+M).
+    """
     return block_matrix(G_old, G_old_new, G_old_new.T, G_new)
 
 def inv_blockmatr(P:Array, P_inv:Array, Q:Array, R:Array, S:Array) -> Array:
